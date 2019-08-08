a42a3c3f7e19f1c9a2810559c2c566d2875d0261
Giang Nguyen <thuygiang152@gmail.com>
2019-08-08 16:58:17 -0700

    directory folder created, update style sheet

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MenuItem = props => {
+  <div className="menu-item">
+    <div className="content">
+      <h1 className="title">{props.title}</h1>
+      <span className="subtitle">SHOP NOW</span>
+    </div>
+  </div>;
+};
+
+export default MenuItem;
