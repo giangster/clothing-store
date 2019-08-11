83ddd8d36fed6e304b44e53c2b74af35df2b8335
Giang Nguyen <thuygiang152@gmail.com>
2019-08-10 22:21:58 -0700

    stlying for menu-item component added

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -9,28 +9,28 @@ class Directory extends Component {
     this.state = {
       sections: [
         {
-          title: "hats",
+          title: "HATS",
           imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
           id: 1
         },
         {
-          title: "jackets",
+          title: "JACKETS",
           imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
           id: 2
         },
         {
-          title: "sneakers",
+          title: "SNEAKERS",
           imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
           id: 3
         },
         {
-          title: "womens",
+          title: "WOMENS",
           imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
           size: "large",
           id: 4
         },
         {
-          title: "mens",
+          title: "MENS",
           imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
           size: "large",
           id: 5
