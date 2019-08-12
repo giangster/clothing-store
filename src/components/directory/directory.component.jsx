f9f101a1c9fb57fd4370a7d36b1cceb85fb24379
Giang Nguyen <thuygiang152@gmail.com>
2019-08-11 23:49:19 -0700

    linkUrl props added in MenuItem component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -11,29 +11,34 @@ class Directory extends Component {
         {
           title: "HATS",
           imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-          id: 1
+          id: 1,
+          linkUrl: "hats"
         },
         {
           title: "JACKETS",
           imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-          id: 2
+          id: 2,
+          linkUrl: ""
         },
         {
           title: "SNEAKERS",
           imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-          id: 3
+          id: 3,
+          linkUrl: ""
         },
         {
           title: "WOMENS",
           imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
           size: "large",
-          id: 4
+          id: 4,
+          linkUrl: ""
         },
         {
           title: "MENS",
           imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
           size: "large",
-          id: 5
+          id: 5,
+          linkUrl: ""
         }
       ]
     };
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
 import "./menu-item.styles.scss";
 
 const MenuItem = ({ title, imageUrl, size }) => (
@@ -14,4 +15,4 @@ const MenuItem = ({ title, imageUrl, size }) => (
   </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
