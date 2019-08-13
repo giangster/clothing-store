9e6a384895b26d95614fe8e250b75c026a37fb09
Giang Nguyen <thuygiang152@gmail.com>
2019-08-12 23:20:32 -0700

    render collection Item

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./collection-item.styles.scss";
 
 const CollectionItem = ({ id, name, imageUrl, price }) => {
+  console.log("collection item ", id);
   return (
     <div className="collection-item">
       <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -9,11 +9,8 @@ const CollectionPreview = ({ title, items }) => {
       <div className="preview">
         {items
           .filter((item, index) => index < 4)
-          .map(item => (
-            <div key={item.id}>
-              {item.name}
-              <CollectionItem key={item.id} {...item.props} />
-            </div>
+          .map(({ id, ...props }) => (
+            <CollectionItem key={id} {...props} />
           ))}
       </div>
     </div>
