04244c219497eef963f250c98b2ab39bf30e058a
Giang Nguyen <thuygiang152@gmail.com>
2019-08-16 11:27:22 -0500

    form added in sign-in component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -9,11 +9,41 @@ class SignIn extends Component {
       password: ""
     };
   }
+
+  onChange = e => {
+    e.preventDefault();
+    this.setState({
+      [e.target.name]: [e.target.value]
+    });
+  };
+
   render() {
     return (
       <div>
         <h2>I already have an account</h2>
         <p>Sign in with your email and password</p>
+
+        <form onSubmit={this.handleChange}>
+          Email: <br />
+          <input
+            required
+            type="email"
+            name="email"
+            value={this.state.email}
+            onChange={this.onChange}
+          />
+          <br />
+          Password: <br />
+          <input
+            required
+            type="password"
+            name="password"
+            value={this.state.password}
+            onChange={this.onChange}
+          />
+          <br />
+          <input type="submit" value="Log in" />
+        </form>
       </div>
     );
   }
--- src/pages/signin-signup/signin-signup.component.jsx
+++ src/pages/signin-signup/signin-signup.component.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "./signin-signup.styles.scss";
+import "./signin-signup.styles.scss";
 import SignIn from "../../components/sign-in/sign-in.component";
 
 class SignInSignUpPage extends Component {
