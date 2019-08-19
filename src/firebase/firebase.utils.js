34e3ee274589b2797118f42485caca8700234782
Giang Nguyen <thuygiang152@gmail.com>
2019-08-18 23:29:22 -0500

    firebase installed

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,13 @@
+import * as firebase from "firebase";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAqDKoHLWptuuXwwHwth2dYJiAX8h6EL4E",
+  authDomain: "clothing-store-082019.firebaseapp.com",
+  databaseURL: "https://clothing-store-082019.firebaseio.com",
+  projectId: "clothing-store-082019",
+  storageBucket: "",
+  messagingSenderId: "937460022374",
+  appId: "1:937460022374:web:409509828b188bb4"
+};
+
+firebase.initializeApp(firebaseConfig);
