c76e01db71c9d80883447416e0210107fe084edf
Giang Nguyen <thuygiang152@gmail.com>
2019-08-19 11:39:54 -0500

    firebase.utils modified

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,3 +11,12 @@ var firebaseConfig = {
 };
 
 firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
