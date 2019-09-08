04cabe42bc018109703f08ef650d23ca291ca4ef
Giang Nguyen <thuygiang152@gmail.com>
2019-09-08 23:34:19 +0300

    Applying mapStateToProps in Header component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./header.styles.scss";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { auth } from "../../firebase/firebase.utils.js";
 
@@ -31,4 +32,7 @@ const Header = ({ currentUser }) => {
   );
 };
 
-export default Header;
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
+export default connect(mapStateToProps)(Header);
