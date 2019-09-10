28413842342ed222ff6e4b2e75f168cdaa496020
Giang Nguyen <thuygiang152@gmail.com>
2019-09-10 23:37:12 +0300

    Adding Cart component

---
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./cart-dropdown.styles.scss";
+
+const Cart = () => (
+  <div className="cart-dropdown">
+    <div className="cart-items"></div>
+  </div>
+);
+
+export default Cart;
