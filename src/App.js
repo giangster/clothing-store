54bb69d0dfc0ee90dda5ed07719efbf3e6b84a1e
Giang Nguyen <thuygiang152@gmail.com>
2019-09-10 16:02:27 +0300

    Fixing currentUser: null mistake, works normally now

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
 
   componentDidMount() {
     const { setCurrentUser } = this.props;
+    console.log(setCurrentUser);
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
--- src/redux/user/user-action.js
+++ src/redux/user/user-action.js
@@ -1,6 +1,6 @@
 export const setCurrentUser = user => {
   return {
-    type: "SET_CURRENT_STATE",
+    type: "SET_CURRENT_USER",
     payload: user
   };
 };
