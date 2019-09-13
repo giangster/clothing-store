f8805a8c15b542a4640e4e999a002b9ab380bbfc
Giang Nguyen <thuygiang152@gmail.com>
2019-09-14 00:01:25 +0300

    Modifying number of items in cart icon from Header

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,10 +4,10 @@ import { connect } from "react-redux";
 import "./cart-icon.styles.scss";
 import { toggleCartStatus } from "../../redux/cart/cart-action";
 
-const CartIcon = ({ toggleCartStatus }) => (
+const CartIcon = ({ toggleCartStatus, cartItems }) => (
   <div className="cart-icon" onClick={toggleCartStatus}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{cartItems.length}</span>
   </div>
 );
 
@@ -15,7 +15,11 @@ const mapDispatchToProps = dispatch => ({
   toggleCartStatus: () => dispatch(toggleCartStatus())
 });
 
+const mapStateToProps = state => ({
+  cartItems: state.cart.cartItems
+});
+
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(CartIcon);
