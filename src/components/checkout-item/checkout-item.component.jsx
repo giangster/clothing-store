38187e501774407a34ce47bfd2201b6f91b86731
Giang Nguyen <thuygiang152@gmail.com>
2019-09-19 00:06:08 +0300

    Adding functionality for remove-item button

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -3,14 +3,15 @@ import "./checkout-item.styles.scss";
 import { connect } from "react-redux";
 import { removeItem } from "../../redux/cart/cart-action";
 
-const CheckoutItem = ({ quantity, name, imageUrl, price }, removeItem) => {
+const CheckoutItem = ({ cartItem, removeItem }) => {
+  const { quantity, name, imageUrl, price } = cartItem;
   return (
     <div className="checkout-item">
       <img className="image-container" alt="item" src={imageUrl}></img>
       <span className="name">{name}</span>
       <span className="quantity">{quantity}</span>
       <span className="price">{price}</span>
-      <div className="remove-button" onClick={removeItem}>
+      <div className="remove-button" onClick={removeItem(cartItem)}>
         &#10005;
       </div>
     </div>
