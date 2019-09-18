89785a3bef8d1136ea5d87d5cb5ff9bfc1d43074
Giang Nguyen <thuygiang152@gmail.com>
2019-09-18 23:58:39 +0300

    Adding functionality for remove-item button

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,16 +1,27 @@
 import React from "react";
 import "./checkout-item.styles.scss";
+import { connect } from "react-redux";
+import { removeItem } from "../../redux/cart/cart-action";
 
-const CheckoutItem = ({ quantity, name, imageUrl, price }) => {
+const CheckoutItem = ({ quantity, name, imageUrl, price }, removeItem) => {
   return (
     <div className="checkout-item">
       <img className="image-container" alt="item" src={imageUrl}></img>
       <span className="name">{name}</span>
       <span className="quantity">{quantity}</span>
       <span className="price">{price}</span>
-      <div className="remove-button">&#10005;</div>
+      <div className="remove-button" onClick={removeItem}>
+        &#10005;
+      </div>
     </div>
   );
 };
 
-export default CheckoutItem;
+const mapDispatchToProps = dispatch => ({
+  removeItem: item => dispatch(removeItem(item))
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(CheckoutItem);
