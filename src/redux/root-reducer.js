93b7986bc6bdc89c70315745ca735c0014c7eddb
Giang Nguyen <thuygiang152@gmail.com>
2019-09-23 19:46:13 +0300

    Wrapping App component in PersistGate, maintaining the state of the add when refreashing

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -7,6 +7,9 @@ import storage from "redux-persist/lib/storage";
 const persistConfig = {
   key: "root",
   storage,
+  //whitelist is an array of strings of the reducer we want to persist.
+  //In this case, there is only the cart reducer, since Firebase handles the user reducer.
+  //In the future, add more reducer that you want to persist to this array.
   whitelist: ["cart"]
 };
 
