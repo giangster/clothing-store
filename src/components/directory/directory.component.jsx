4818d61fab01a6b5174513be10cfd7b67c812045
Giang Nguyen <thuygiang152@gmail.com>
2019-09-24 22:21:06 +0300

    Using diretory selector from Directory component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import MenuItem from "../menu-item/menu-item.component";
 import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { selectDirectorySections } from "../../redux/directory/directory-selectors";
 
 import "./directory.styles.scss";
 
@@ -14,4 +16,8 @@ const Directory = ({ sections }) => {
   );
 };
 
-export default Directory;
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections
+});
+
+export default connect(mapStateToProps)(Directory);
