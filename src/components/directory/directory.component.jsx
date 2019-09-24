714c4ea922d88a41fd2f641ad6d559bee8036dfc
Giang Nguyen <thuygiang152@gmail.com>
2019-09-24 22:08:43 +0300

    Changing Directory to functional component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,58 +1,17 @@
 import React, { Component } from "react";
 import MenuItem from "../menu-item/menu-item.component";
+import { connect } from "react-redux";
+
 import "./directory.styles.scss";
 
-class Directory extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      sections: [
-        {
-          title: "HATS",
-          imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-          id: 1,
-          linkUrl: "hats"
-        },
-        {
-          title: "JACKETS",
-          imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-          id: 2,
-          linkUrl: ""
-        },
-        {
-          title: "SNEAKERS",
-          imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-          id: 3,
-          linkUrl: ""
-        },
-        {
-          title: "WOMENS",
-          imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-          size: "large",
-          id: 4,
-          linkUrl: ""
-        },
-        {
-          title: "MENS",
-          imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-          size: "large",
-          id: 5,
-          linkUrl: ""
-        }
-      ]
-    };
-  }
-
-  render() {
-    return (
-      <div className="directory-menu">
-        {this.state.sections.map(({ id, ...otherProps }) => (
-          <MenuItem key={id} {...otherProps} />
-        ))}
-      </div>
-    );
-  }
-}
+const Directory = ({ sections }) => {
+  return (
+    <div className="directory-menu">
+      {sections.map(({ id, ...otherProps }) => (
+        <MenuItem key={id} {...otherProps} />
+      ))}
+    </div>
+  );
+};
 
 export default Directory;
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -3,6 +3,7 @@ import userReducer from "./user/user-reducer";
 import cartReducer from "./cart/cart-reducer";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+import directoryReducer from "./directory/directory.reducer";
 
 const persistConfig = {
   key: "root",
@@ -10,12 +11,13 @@ const persistConfig = {
   //whitelist is an array of strings of the reducer we want to persist.
   //In this case, there is only the cart reducer, since Firebase handles the user reducer.
   //In the future, add more reducer that you want to persist to this array.
-  whitelist: ["cart"]
+  whitelist: ["cart", "directory"]
 };
 
 const rootReducer = combineReducers({
   user: userReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  directory: directoryReducer
 });
 
 export default persistReducer(persistConfig, rootReducer);
