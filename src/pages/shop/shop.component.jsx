1fa759c2aea1aa5d5c395ba89e181b080bf13dfd
Giang Nguyen <thuygiang152@gmail.com>
2019-09-24 22:56:13 +0300

    Using shop selector in Shop component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,16 +1,13 @@
-import React, { Component } from "react";
+import React from "react";
 import CollectionPreview from "../../components/collection-preview/collection-preview.component.jsx";
 
-class ShopPage extends Component {
-  render() {
-    let collections = [...this.state.collections];
-    return (
-      <div>
-        {collections.map(({ id, ...otherProps }) => {
-          return <CollectionPreview key={id} {...otherProps} />;
-        })}
-      </div>
-    );
-  }
-}
+const ShopPage = ({ collections }) => {
+  return (
+    <div>
+      {collections.map(({ id, ...otherProps }) => {
+        return <CollectionPreview key={id} {...otherProps} />;
+      })}
+    </div>
+  );
+};
 export default ShopPage;
