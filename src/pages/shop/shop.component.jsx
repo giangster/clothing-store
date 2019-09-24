711650593bc84714db97e62961b6a1c47465bdfc
Giang Nguyen <thuygiang152@gmail.com>
2019-09-24 22:56:20 +0300

    Using shop selector in Shop component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import CollectionPreview from "../../components/collection-preview/collection-preview.component.jsx";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { selectCollections } from "../../redux/shop/shop-selectors";
 
 const ShopPage = ({ collections }) => {
   return (
@@ -10,4 +13,9 @@ const ShopPage = ({ collections }) => {
     </div>
   );
 };
-export default ShopPage;
+
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections
+});
+
+export default connect(mapStateToProps)(ShopPage);
--- src/redux/shop/shop-selectors.js
+++ src/redux/shop/shop-selectors.js
@@ -2,7 +2,7 @@ import { createSelector } from "reselect";
 
 const selectShop = state => state.shop;
 
-export const selectDirectorySections = createSelector(
+export const selectCollections = createSelector(
   [selectShop],
   shop => shop.collections
 );
