7a0f81212d86f95dab854cbe3f98bff144b8b24b
Giang Nguyen <thuygiang152@gmail.com>
2019-09-24 22:52:36 +0300

    Implementing the same approach with shop_data:  creating its own reducer

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,12 +1,7 @@
 import React, { Component } from "react";
-import { SHOP_DATA } from "./shop.data.jsx";
 import CollectionPreview from "../../components/collection-preview/collection-preview.component.jsx";
 
 class ShopPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { collections: SHOP_DATA };
-  }
   render() {
     let collections = [...this.state.collections];
     return (
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -4,6 +4,7 @@ import cartReducer from "./cart/cart-reducer";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import directoryReducer from "./directory/directory-reducer";
+import shopReducer from "./shop/shop-reducer";
 
 const persistConfig = {
   key: "root",
@@ -11,13 +12,14 @@ const persistConfig = {
   //whitelist is an array of strings of the reducer we want to persist.
   //In this case, there is only the cart reducer, since Firebase handles the user reducer.
   //In the future, add more reducer that you want to persist to this array.
-  whitelist: ["cart", "directory"]
+  whitelist: ["cart", "directory", "shop"]
 };
 
 const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
-  directory: directoryReducer
+  directory: directoryReducer,
+  shop: shopReducer
 });
 
 export default persistReducer(persistConfig, rootReducer);
