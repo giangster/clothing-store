faf912e9552b735197a9ca05ca051ae88bec75a9
Giang Nguyen <thuygiang152@gmail.com>
2019-09-25 23:26:30 +0300

    Nested routing in Shop Page

--- src/components/collection-overview/collection-overview.component.jsx
+++ src/components/collection-overview/collection-overview.component.jsx
@@ -1,20 +1,18 @@
 import React from "react";
 
-import CollectionPreview from "../../components/collection-preview/collection-preview.component.jsx";
+import CollectionPreview from "../collection-preview/collection-preview.component.jsx";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import { selectCollections } from "../../redux/shop/shop-selectors";
 import "./collection-overview.styles.scss";
 
-const CollectionOverview = ({ collections }) => {
-  return (
-    <div className="collections-overview">
-      {collections.map(({ id, ...otherProps }) => {
-        return <CollectionPreview key={id} {...otherProps} />;
-      })}
-    </div>
-  );
-};
+const CollectionOverview = ({ collections }) => (
+  <div className="collection-overview">
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
+    ))}
+  </div>
+);
 
 const mapStateToProps = createStructuredSelector({
   collections: selectCollections
---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./collection.styles.scss";
+
+const CollectionPage = ({ match }) => (
+  <div className="category">
+    <h2>COLLECTION PAGE</h2>
+  </div>
+);
+
+export default CollectionPage;
