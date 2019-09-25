d4b485eda0babafb5048c3c8daac01b6cf232c99
Giang Nguyen <thuygiang152@gmail.com>
2019-09-25 23:31:33 +0300

    Renaming cont for better synchronism

--- src/redux/shop/shop-reducer.js
+++ src/redux/shop/shop-reducer.js
@@ -1,10 +1,10 @@
 import { SHOP_DATA } from "./shop-data";
 
-const INITIAL_STATE = {
+const ORIGIN_STATE = {
   collections: SHOP_DATA
 };
 
-const shopReducer = (state = INITIAL_STATE, action) => {
+const shopReducer = (state = ORIGIN_STATE, action) => {
   switch (action.type) {
     default:
       return state;
