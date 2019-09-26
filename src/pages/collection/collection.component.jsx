732e79105fd5bbd7d13c8c7ab268469544175eea
Giang Nguyen <thuygiang152@gmail.com>
2019-09-26 23:42:26 +0300

    Apply selector in collection component

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-
 import "./collection.styles.scss";
+import { selectCollection } from "../../redux/shop/shop-selectors";
+import { connect } from "react-redux";
 
 const CollectionPage = ({ match }) => (
   <div className="category">
@@ -8,4 +9,8 @@ const CollectionPage = ({ match }) => (
   </div>
 );
 
-export default CollectionPage;
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
+
+export default connect(mapStateToProps)(CollectionPage);
