2ac2209e6b71a27194942420881a43f198307b6d
Giang Nguyen <thuygiang152@gmail.com>
2019-09-30 00:44:01 +0300

    Adding new selector to make Collection Overview page works

--- src/components/collection-overview/collection-overview.component.jsx
+++ src/components/collection-overview/collection-overview.component.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import CollectionPreview from "../collection-preview/collection-preview.component";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
-import { selectCollections } from "../../redux/shop/shop-selectors";
+import { selectCollectionsForPreview } from "../../redux/shop/shop-selectors";
 import "./collection-overview.styles.scss";
 
 const CollectionOverview = ({ collections }) => {
@@ -17,7 +17,7 @@ const CollectionOverview = ({ collections }) => {
 };
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections
+  collections: selectCollectionsForPreview
 });
 
 export default connect(mapStateToProps)(CollectionOverview);
