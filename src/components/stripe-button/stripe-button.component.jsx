8501c00cbdc0bdcc08c587362962fd04bbfcb29b
Giang Nguyen <thuygiang152@gmail.com>
2019-09-30 22:21:56 +0300

    Modifying payment button

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -3,6 +3,12 @@ import StripeCheckout from "react-stripe-checkout";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
+  const publishableKey = "pk_test_O3X4CTzJZzpYKwEF1qMD78VF00WOj5fSLH";
+  const onToken = token => {
+    console.log(token);
+    alert("Payment succesful!");
+  };
+
   return (
     <div>
       <StripeCheckout
@@ -11,8 +17,13 @@ const StripeCheckoutButton = ({ price }) => {
         description={`Your total is €${price}`}
         amount={priceForStripe}
         currency="EUR"
+        stripeKey={publishableKey}
         shippingAddress
         billingAddress
+        token={onToken}
+        alipay
+        bitcoin
+        allowRememberMe
       />
     </div>
   );
