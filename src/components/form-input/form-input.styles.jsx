9f867d8c540e796fae8efb0897cc2a749db9b244
Giang Nguyen <thuygiang152@gmail.com>
2019-10-06 20:50:35 +0300

    Fixing FormInput style

--- src/components/form-input/form-input.styles.jsx
+++ src/components/form-input/form-input.styles.jsx
@@ -21,21 +21,21 @@ export const GroupContainer = styled.div`
 export const FormInputContainer = styled.input`
   background: none;
   background-color: white;
-  color: $sub-color;
+  color: ${subColor};
   font-size: 18px;
   padding: 10px 10px 10px 5px;
   display: block;
   width: 100%;
   border: none;
   border-radius: 0;
-  border-bottom: 1px solid $sub-color;
+  border-bottom: 1px solid ${subColor};
   margin: 25px 0;
 
   &:focus {
     outline: none;
   }
 
-  &:focus ~ .form-input-label {
+  &:focus ~ label {
     ${shrinkLabelStyles};
   }
 `;
