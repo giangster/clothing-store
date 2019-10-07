e2e672f6d5d22faa59e910097b2d9a701224d46f
Giang Nguyen <thuygiang152@gmail.com>
2019-10-07 19:35:36 +0300

    Pulling data from firestore to the application

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,8 +2,21 @@ import React from "react";
 import { Route } from "react-router-dom";
 import CollectionOverview from "../../components/collection-overview/collection-overview.component.jsx";
 import CollectionPage from "../collection/collection.component.jsx";
+import {
+  firestore,
+  convertCollectionsSnapshotToMap
+} from "../../firebase/firebase.utils";
 
 class ShopPage extends React.Component {
+  unsubscribeFromSnapshot = null;
+  componentDidMount() {
+    const collectionRef = firestore.collection("collections");
+
+    collectionRef.onSnapShot(async snapShot => {
+      convertCollectionsSnapshotToMap(snapShot);
+    });
+  }
+
   render() {
     const { match } = this.props;
     return (
