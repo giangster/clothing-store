92393d54e4024bc6772457f6e3f3d5456b3b19a3
Giang Nguyen <thuygiang152@gmail.com>
2019-10-07 19:16:39 +0300

    Switching ShopPage to class component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -3,11 +3,19 @@ import { Route } from "react-router-dom";
 import CollectionOverview from "../../components/collection-overview/collection-overview.component.jsx";
 import CollectionPage from "../collection/collection.component.jsx";
 
-const ShopPage = ({ match }) => (
-  <div className="shop-page">
-    <Route exact path={`${match.path}`} component={CollectionOverview} />
-    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-  </div>
-);
+class ShopPage extends React.Component {
+  render() {
+    const { match } = this.props;
+    return (
+      <div className="shop-page">
+        <Route exact path={`${match.path}`} component={CollectionOverview} />
+        <Route
+          path={`${match.path}/:collectionId`}
+          component={CollectionPage}
+        />
+      </div>
+    );
+  }
+}
 
 export default ShopPage;
