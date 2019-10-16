750cfaac80fa1b67be17c793ba8464f72905e475
Giang Nguyen <thuygiang152@gmail.com>
2019-10-16 23:52:55 +0300

    Using compose to make code extensible later

--- src/components/collection-overview/collection-overview.container.jsx
+++ src/components/collection-overview/collection-overview.container.jsx
@@ -1,5 +1,6 @@
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
+import { compose } from "redux";
 
 import WithSpinner from "../../components/with-spinner/with-spinner.component.jsx";
 import { selectIsCollectionFetching } from "../../redux/shop/shop-selectors.js";
@@ -9,6 +10,12 @@ const mapStateToProps = createStructuredSelector({
   isLoading: selectIsCollectionFetching
 });
 
-export const CollectionOverviewContainer = connect(mapStateToProps)(
-  WithSpinner(CollectionOverview)
-);
+//In here, compose is used instead of the regular way to ensure that in the future, if there is more HOC than just WithSpinner, the code would not look like a mess
+export const CollectionOverviewContainer = compose(
+  connect(mapStateToProps),
+  WithSpinner
+)(CollectionOverview);
+
+// export const CollectionOverviewContainer = connect(mapStateToProps)(
+//   WithSpinner(CollectionOverview)
+// );
