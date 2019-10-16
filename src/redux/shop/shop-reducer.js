d21462ac6296c1f45f0f00508495bb16b62e3bf9
Giang Nguyen <thuygiang152@gmail.com>
2019-10-16 11:54:04 +0300

    Implementing redux thunk to move the fetching to reducer

--- src/redux/shop/shop-reducer.js
+++ src/redux/shop/shop-reducer.js
@@ -1,7 +1,9 @@
 import ShopActionTypes from "./shop-types";
 
 const ORIGIN_STATE = {
-  collections: null
+  collections: null,
+  isFetching: false,
+  message: ""
 };
 
 const shopReducer = (state = ORIGIN_STATE, action) => {
@@ -9,7 +11,19 @@ const shopReducer = (state = ORIGIN_STATE, action) => {
     case ShopActionTypes.FETCH_COLLECTIONS_START:
       return {
         ...state,
+        isFetching: true
+      };
+    case ShopActionTypes.FETCH_COLLECTIONS_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
         collections: action.payload
+      };
+    case ShopActionTypes.FETCH_COLLECTIONS_FAILURE:
+      return {
+        ...state,
+        isFetching: false,
+        message: action.payload
       };
     default:
       return state;
