0e2bd8cbfa17976aa9a79c8918a4ae1f2c3cbe15
Giang Nguyen <thuygiang152@gmail.com>
2019-10-21 22:06:21 +0300

    Implementing redux-saga: using generators to fetch data to shop component

--- src/redux/shop/shop-sagas.js
+++ src/redux/shop/shop-sagas.js
@@ -1,9 +1,27 @@
-import { takeEvery } from "redux-saga/effects";
+import { takeEvery, call, put } from "redux-saga/effects";
+import firestore from "./../../firebase/firebase.utils";
 
 import ShopActionTypes from "./shop-types";
+import { convertCollectionsSnapshotToMap } from "../../firebase/firebase.utils";
+import {
+  fetchCollectionsSuccess,
+  fetchCollectionsFailure
+} from "./shop-actions";
 
 export function* fetchCollectionsAsync() {
   yield console.log("Look at meeeeeeee");
+  try {
+    const collectionRef = firestore.collection("collections");
+    const snapshot = yield collectionRef.get();
+    const collectionsMap = yield call(
+      convertCollectionsSnapshotToMap,
+      snapshot
+    );
+
+    yield put(fetchCollectionsSuccess(collectionsMap));
+  } catch (err) {
+    yield put(fetchCollectionsFailure(err.message));
+  }
 }
 
 export function* fetchCollectionsStart() {
