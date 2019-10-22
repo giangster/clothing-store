6be93b940c1aa23d9ca6405266e691b3fa01f976
Giang Nguyen <thuygiang152@gmail.com>
2019-10-22 21:01:05 +0300

    Refactoring user action using Redux-Saga: Adding new types, creating actions

--- src/redux/user/user-action.js
+++ src/redux/user/user-action.js
@@ -6,3 +6,27 @@ export const setCurrentUser = user => {
     payload: user
   };
 };
+
+export const googleSignInStart = () => {
+  return { type: UserActionTypes.GOOGLE_SIGN_IN_START };
+};
+
+export const googleSignInSuccess = user => {
+  return { type: UserActionTypes.GOOGLE_SIGN_IN_SUCCESS, payload: user };
+};
+
+export const googleSignInFailure = err => {
+  return { type: UserActionTypes.GOOGLE_SIGN_IN_FAILURE, payload: err };
+};
+
+export const emailSignInStart = () => {
+  return { type: UserActionTypes.EMAIL_SIGN_IN_START };
+};
+
+export const emailSignInSuccess = user => {
+  return { type: UserActionTypes.EMAIL_SIGN_IN_SUCCESS, payload: user };
+};
+
+export const emailSignInFailure = err => {
+  return { type: UserActionTypes.EMAIL_SIGN_IN_FAILURE, payload: err };
+};
