d339b8f1fe6ad714e23d97fac7a7cd9c7dd087f6
Giang Nguyen <thuygiang152@gmail.com>
2019-10-22 21:35:40 +0300

    Renaming file for better convention, adding new types for action and modifying the user reducer to get ready for saga

---
+++ src/redux/user/user-actions.js
@@ -0,0 +1,35 @@
+import { UserActionTypes } from "./user-types.js";
+
+export const setCurrentUser = user => {
+  return {
+    type: UserActionTypes.SET_CURRENT_USER,
+    payload: user
+  };
+};
+
+export const googleSignInStart = () => {
+  return { type: UserActionTypes.GOOGLE_SIGN_IN_START };
+};
+
+export const googleSignInSuccess = user => {
+  return { type: UserActionTypes.GOOGLE_SIGN_IN_SUCCESS, payload: user };
+};
+
+export const googleSignInFailure = err => {
+  return { type: UserActionTypes.GOOGLE_SIGN_IN_FAILURE, payload: err };
+};
+
+export const emailSignInStart = () => {
+  return { type: UserActionTypes.EMAIL_SIGN_IN_START };
+};
+
+export const emailSignInSuccess = emailAndPassword => {
+  return {
+    type: UserActionTypes.EMAIL_SIGN_IN_SUCCESS,
+    payload: emailAndPassword
+  };
+};
+
+export const emailSignInFailure = err => {
+  return { type: UserActionTypes.EMAIL_SIGN_IN_FAILURE, payload: err };
+};
--- src/redux/user/user-reducer.js
+++ src/redux/user/user-reducer.js
@@ -1,15 +1,25 @@
 import { UserActionTypes } from "./user-types.js";
 
 const ORIGIN_STATE = {
-  currentUser: null
+  currentUser: null,
+  message: ""
 };
 
 const userReducer = (state = ORIGIN_STATE, action) => {
   switch (action.type) {
-    case UserActionTypes.SET_CURRENT_USER:
+    //Stacking cases is doable
+    case UserActionTypes.GOOGLE_SIGN_IN_SUCCESS:
+    case UserActionTypes.EMAIL_SIGN_IN_SUCCESS:
       return {
         ...state,
-        currentUser: action.payload
+        currentUser: action.payload,
+        message: null
+      };
+    case UserActionTypes.GOOGLE_SIGN_IN_FAILURE:
+    case UserActionTypes.EMAIL_SIGN_IN_FAILURE:
+      return {
+        ...state,
+        message: action.payload
       };
     default:
       return state;
