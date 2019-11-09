bfe4862a9de078a835601089ed517b4eea04a9a8
Giang Nguyen <thuygiang152@gmail.com>
2019-11-09 16:48:29 +0700

    Using Hooks: Converting Sign Up component with useState

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -1,32 +1,27 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 import { connect } from "react-redux";
 import { SignUpContainer, SignUpTitle } from "./sign-up.styles";
 import { signUpStart } from "../../redux/user/user-actions";
 
-class SignUp extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      displayName: "",
-      email: "",
-      password: "",
-      confirmPassword: ""
-    };
-  }
+const SignUp = ({ signUpStart }) => {
+  const [userCredentials, setCredentials] = useState({
+    displayName: "",
+    email: "",
+    password: "",
+    confirmPassword: ""
+  });
 
-  onChange = e => {
+  const { displayName, email, password, confirmPassword } = userCredentials;
+
+  const onChange = e => {
     e.preventDefault();
-    this.setState({
-      [e.target.name]: e.target.value
-    });
+    setCredentials({ ...userCredentials, [e.target.name]: e.target.value });
   };
 
-  onSubmit = async e => {
+  const onSubmit = async e => {
     e.preventDefault();
-    const { signUpStart } = this.props;
-    const { displayName, email, password, confirmPassword } = this.state;
 
     if (password !== confirmPassword) {
       alert("Password doesn't match");
@@ -36,52 +31,49 @@ class SignUp extends Component {
     signUpStart({ displayName, email, password });
   };
 
-  render() {
-    const { displayName, email, password, confirmPassword } = this.state;
-    return (
-      <SignUpContainer>
-        <SignUpTitle>I don't have an account</SignUpTitle>
-        <p>Sign up easily now!</p>
+  return (
+    <SignUpContainer>
+      <SignUpTitle>I don't have an account</SignUpTitle>
+      <p>Sign up easily now!</p>
 
-        <form className="sign-up-form" onSubmit={this.onSubmit}>
-          <FormInput
-            required
-            type="text"
-            name="displayName"
-            label="Display Name"
-            value={displayName}
-            onChange={this.onChange}
-          />
-          <FormInput
-            required
-            type="email"
-            name="email"
-            label="Email"
-            value={email}
-            onChange={this.onChange}
-          />
-          <FormInput
-            required
-            type="password"
-            name="password"
-            label="Password"
-            value={password}
-            onChange={this.onChange}
-          />
-          <FormInput
-            required
-            type="password"
-            name="confirmPassword"
-            label="Confirm Password"
-            value={confirmPassword}
-            onChange={this.onChange}
-          />
-          <CustomButton type="submit">Sign up</CustomButton>
-        </form>
-      </SignUpContainer>
-    );
-  }
-}
+      <form className="sign-up-form" onSubmit={onSubmit}>
+        <FormInput
+          required
+          type="text"
+          name="displayName"
+          label="Display Name"
+          value={displayName}
+          onChange={onChange}
+        />
+        <FormInput
+          required
+          type="email"
+          name="email"
+          label="Email"
+          value={email}
+          onChange={onChange}
+        />
+        <FormInput
+          required
+          type="password"
+          name="password"
+          label="Password"
+          value={password}
+          onChange={onChange}
+        />
+        <FormInput
+          required
+          type="password"
+          name="confirmPassword"
+          label="Confirm Password"
+          value={confirmPassword}
+          onChange={onChange}
+        />
+        <CustomButton type="submit">Sign up</CustomButton>
+      </form>
+    </SignUpContainer>
+  );
+};
 
 const mapDispatchToProps = dispatch => ({
   signUpStart: user => dispatch(signUpStart(user))
