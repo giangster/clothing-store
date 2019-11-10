e8b25760b0e30471ad827d9bd23b732cfb0db4fa
Giang Nguyen <thuygiang152@gmail.com>
2019-11-10 08:55:01 +0700

    Using Hooks: Converting main App component with useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component.jsx";
