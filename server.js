4df3d0265081996ba6b5682b6d9a154bfc5286f1
Giang Nguyen <thuygiang152@gmail.com>
2019-11-11 23:20:48 +0700

    Stripe payment backend: building a basic server

--- server.js
+++ server.js
@@ -7,3 +7,23 @@ if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors());
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
+
+app.listen(port, err => {
+  if (err) {
+    throw err;
+  }
+  console.log(`Server running on port ${port}`);
+});
