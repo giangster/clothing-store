248556bf50547dd29035eb02f79a4e84231ce541
Giang Nguyen <thuygiang152@gmail.com>
2020-01-04 00:26:41 +0200

    Responsiveness: modifying stylings of sign in page

--- client/src/pages/signin-signup/signin-signup.styles.jsx
+++ client/src/pages/signin-signup/signin-signup.styles.jsx
@@ -5,4 +5,9 @@ export const SigninSignupContainer = styled.div`
   display: flex;
   justify-content: space-between;
   margin: 30px;
+
+  @media screen and (max-width: 600px) {
+    width: 450px;
+    display: block;
+  }
 `;
